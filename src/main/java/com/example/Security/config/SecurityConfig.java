6d65b18e4278db11fe43f24a05dd862429e465c5
sahan1997dilshan <dsahan548@gmail.com>
2025-12-22 10:59:27 +0530

    added first security config file

---
+++ src/main/java/com/example/Security/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.Security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+      http
+              .csrf(csrf -> csrf.disable())
+              .authorizeHttpRequests(auth -> auth
+                      .requestMatchers("/api/v1/account/my-account").authenticated()
+                      .anyRequest().permitAll()
+              )
+              .httpBasic(Customizer.withDefaults());
+
+      return http.build();
+
+
+    }
+}
